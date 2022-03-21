d47f65e94b8a695e367b311f499ce3d4e6bcedcb
James Newton-King <james@newtonking.com>
2022-03-21 11:05:05 +0800

    Rebase

--- src/Grpc.AspNetCore.Server/Internal/HttpContextStreamWriter.cs
+++ src/Grpc.AspNetCore.Server/Internal/HttpContextStreamWriter.cs
@@ -38,7 +38,7 @@ namespace Grpc.AspNetCore.Server.Internal
             _writeLock = new object();
         }
 
-        public WriteOptions WriteOptions
+        public WriteOptions? WriteOptions
         {
             get => _context.WriteOptions;
             set => _context.WriteOptions = value;
@@ -49,11 +49,13 @@ namespace Grpc.AspNetCore.Server.Internal
             return WriteCoreAsync(message, CancellationToken.None);
         }
 
+#if NET5_0_OR_GREATER
         // Explicit implementation because this WriteAsync has a default interface implementation.
         Task IAsyncStreamWriter<TResponse>.WriteAsync(TResponse message, CancellationToken cancellationToken)
         {
             return WriteCoreAsync(message, cancellationToken);
         }
+#endif
 
         private async Task WriteCoreAsync(TResponse message, CancellationToken cancellationToken)
         {
