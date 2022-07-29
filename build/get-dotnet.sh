68dc9b1c8092f16f759c765002fdb762c1435668
James Newton-King <james@newtonking.com>
2022-07-29 20:24:59 +0800

    Update SDK

--- build/get-dotnet.sh
+++ build/get-dotnet.sh
@@ -18,7 +18,7 @@ ensure_dir() {
 # main
 
 # resolve SDK version
-sdk_version="7.0.100-preview.7.22327.3"
+sdk_version="7.0.100-rc.1.22378.8"
 sdk_version=$(jq -r .sdk.version $global_json_path)
 
 # download dotnet-install.sh
